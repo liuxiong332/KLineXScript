35960616f5fab3bf431a7342f8a05b882038f351
liuxiong <liuxiong332@163.com>
2019-12-06 08:39:30 +0800

    Add MACD script test

--- tests/data_src_tests.rs
+++ tests/data_src_tests.rs
@@ -131,3 +131,69 @@ fn for_range_test() {
 
     assert_eq!(datasrc.run(data), Ok(()));
 }
+
+const EMA_SCRIPT: &str = "
+pine_ema(x, y) =>
+    alpha = 2 / (y + 1)
+    sum = 0.0
+    sum := alpha * x + (1 - alpha) * (sum[1] ? sum[1] : 0)
+    sum
+print(pine_ema(close, 3.0))
+";
+
+#[test]
+fn ema_test() {
+    struct MyCallback;
+    impl Callback for MyCallback {
+        fn print(&self, _str: String) {
+            assert_eq!(_str, String::from("1,2.5"));
+        }
+    }
+
+    let mut func_block = pine::parse_all(EMA_SCRIPT).unwrap();
+    let inner_vars = libs::declare_vars();
+    let mut datasrc = DataSrc::new(&mut func_block, inner_vars, &MyCallback);
+
+    let mut data = HashMap::new();
+    data.insert("close", vec![Some(2f64), Some(4f64)]);
+
+    assert_eq!(datasrc.run(data), Ok(()));
+}
+
+const MACD_SCRIPT: &str = "
+pine_ema(x, y) =>
+    alpha = 2 / (y + 1)
+    sum = 0.0
+    sum := alpha * x + (1 - alpha) * (sum[1] ? sum[1] : 0)
+    sum
+
+pine_macd() => 
+    // DIF=EMA_{{(close,12)}}-EMA_{{(close,26)}}
+    dif = pine_ema(close, 12.0) - pine_ema(close, 26.0)
+    // DEM=EMA_{{(DIF,9)}}
+    dem = pine_ema(dif, 9.0)
+    //OSC=DIF-DEM=DIF-MACD
+    osc = dif - dem
+    osc
+
+print(pine_macd())
+";
+
+#[test]
+fn macd_test() {
+    struct MyCallback;
+    impl Callback for MyCallback {
+        fn print(&self, _str: String) {
+            assert_eq!(_str, String::from("12.763532763532766,32.82882444136006"));
+        }
+    }
+
+    let mut func_block = pine::parse_all(MACD_SCRIPT).unwrap();
+    let inner_vars = libs::declare_vars();
+    let mut datasrc = DataSrc::new(&mut func_block, inner_vars, &MyCallback);
+
+    let mut data = HashMap::new();
+    data.insert("close", vec![Some(200f64), Some(400f64)]);
+
+    assert_eq!(datasrc.run(data), Ok(()));
+}

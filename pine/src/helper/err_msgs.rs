b98709d47ab1fb424bf7c176261369b10f54fafc
liuxiong332 <liuxiong332@163.com>
2020-05-20 09:06:12 +0800

    Change some doc

--- pine/src/helper/err_msgs.rs
+++ pine/src/helper/err_msgs.rs
@@ -1,7 +1,7 @@
-pub const EXP_VAL_BUT_GET_VAL: &'static str = "Expect {}, But get {}.";
-pub const NO_FIELD_IN_OBJECT: &'static str = "No field named {} found for {}.";
-pub const REQUIRED_PARAMETERS: &'static str = "Required parameters {}.";
+pub const EXP_VAL_BUT_GET_VAL: &'static str = "{} is expected, not a {}.";
+pub const NO_FIELD_IN_OBJECT: &'static str = "No property named {} found in {}.";
+pub const REQUIRED_PARAMETERS: &'static str = "Parameters {} is required.";
 pub const UNRECONGNIZED_TZ: &'static str = "Unrecognized timezone {}.";
 pub const UNRECONGNIZED_RES: &'static str = "Unrecognized resolution {}.";
 pub const GE_1: &'static str = "The value of {} should be greater than or equal to 1.";
-pub const INPUT_SRCS: &'static str = "The input source should only be one of {}.";
+pub const INPUT_SRCS: &'static str = "The input source should be one of {}.";
